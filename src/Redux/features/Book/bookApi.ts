0e079d1acbeaea036c94c65362053284ffaffb81
sufiansar <sufiancodecrush@gamil.com>
2025-07-05 08:38:31 +0600

    update some Responsive Design

--- src/Redux/features/Book/bookApi.ts
+++ src/Redux/features/Book/bookApi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const bookApi = createApi({
   reducerPath: "bookApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000/api" }),
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_API_URL }),
   tagTypes: ["Book"],
   endpoints: (builder) => ({
     getBooks: builder.query({
--- src/Redux/features/Borrow/borrowApi.ts
+++ src/Redux/features/Borrow/borrowApi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const brrowApi = createApi({
   reducerPath: "borrowApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000/api" }),
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_API_URL }),
   tagTypes: ["Borrow"],
   endpoints: (builder) => ({
     borrowBook: builder.mutation({
